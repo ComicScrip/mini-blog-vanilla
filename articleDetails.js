0b83fb5c979c886eea21eb77166db2a6db708bd4
Pierre Genthon <pierre.genthon@wildcodeschool.com>
2022-03-21 14:22:13 +0100

    remove lc content

--- articleDetails.js
+++ articleDetails.js
@@ -1,11 +1,1 @@
-// getting the id of the article we want to show from the URL
-const articleIdStr = window.location.search.replace('?articleId=', '');
-const articleId = parseInt(articleIdStr, 10);
-// Getting the article's information by searching the list of saved articles
-const article = articleList.find((art) => art.id === articleId);
-// retriving DOM elements that will contain the data of the article
-const articleTitleElement = document.querySelector('#articleTitle');
-const articleContentElement = document.querySelector('#articleContent');
-// filling the elements
-articleTitleElement.innerHTML = article.title;
-articleContentElement.innerHTML = article.content;
+
